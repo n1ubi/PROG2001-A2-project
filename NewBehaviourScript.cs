20ecabbb0124095ec2a6d688445d44110a21c263
123hanwwww <3241657311@qq.com>
2026-04-17 17:21:28 +0800

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadMyScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Update is called once per frame
+    public void LoadMyScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
